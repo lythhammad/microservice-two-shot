a1c25ac4e2c9fb0346ec07fb0100368ea2f9d062
Mo <kanawimou@gmail.com>
2023-04-19 14:39:09 -0700

    shoes models and views

--- shoes/api/shoes_rest/models.py
+++ shoes/api/shoes_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class BinVO(models.Model):
+    closet_name = models.CharField(max_length=100)
+    bin_number = models.PositiveSmallIntegerField()
+    bin_size = models.PositiveSmallIntegerField()
+    import_href = models.CharField(max_length=200,unique = True)
+
+class Shoe(models.Model):
+    manufacturer = models.CharField(max_length=100)
+    model_name = models.CharField(max_length=100)
+    picture_url = models.URLField(null=True)
+    color = models.CharField(max_length=100)
+    bin = models.ForeignKey(
+        BinVO,
+        related_name = "shoes",
+        on_delete=models.CASCADE,
+    )
+
+def __str__(self):
+    return self.name
+
+def get_api_url(self):
+    return reverse("api_show_shoe", kwargs={"pk": self.pk})
--- shoes/api/shoes_rest/views.py
+++ shoes/api/shoes_rest/views.py
@@ -1,3 +1,66 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
+from common.json import ModelEncoder
+from .models import Shoe, BinVO
 
-# Create your views here.
+class BinVODetailEncoder(ModelEncoder):
+    model = BinVO
+    property = [
+        "closet_name",
+        "bin_number",
+        "bin_size",
+        "import_href",
+    ]
+class ShoeListEncoder(ModelEncoder):
+    model = Shoe
+    property = [
+        "id",
+        "manufacturer",
+        "name",
+    ]
+def get_extra_data(self, o):
+    return {"bin": o.bin.bin_number}
+
+class ShoeDetailEncoder(ModelEncoder):
+    model = Shoe
+    properties = [
+        "manufacturer",
+        "model_name",
+        "color",
+        "picture_url"
+        "bin"
+    ]
+
+    encoders = {
+        "bin": BinVODetailEncoder()
+    }
+
+
+@require_http_methods(["GET", "POST"])
+def api_list_shoes(request, bin_vo_id=None):
+    if request.method == "GET":
+        if bin_vo_id is not None:
+            shoes = Shoe.objects.filter()
+        return JsonResponse(
+            {"shoes":shoes},
+            encoder=ShoeListEncoder
+        )
+    else:
+        content = json.loads(request.body)
+        try:
+            bin_href = content["bin"]
+            bin = BinVO.objects.get(import_href=bin_href)
+            content["bin"] = bin
+        except BinVO.DoesNotExist:
+            return JsonResponse(
+                {"message": "Invalid bin id"},
+                status=400,
+            )
+        shoe = Shoe.objects.create(**content)
+        return JsonResponse(
+            shoe,
+            encoder=ShoeDetailEncoder,
+            safe = False
+        )
