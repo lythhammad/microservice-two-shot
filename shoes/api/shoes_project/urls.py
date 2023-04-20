147077c63fee0a9227afd654df1dafa206925a6e
Mo <kanawimou@gmail.com>
2023-04-19 21:57:58 -0700

    fixing shoe api

--- shoes/api/shoes_project/urls.py
+++ shoes/api/shoes_project/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import api_list_shoes, api_show_shoe
 
 urlpatterns = [
     path('admin/', admin.site.urls),
